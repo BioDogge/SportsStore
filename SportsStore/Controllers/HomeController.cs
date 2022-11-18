2025e9e58aa56ccaa6a7dc541686b054e42f5a93
Алексей Манаев <manaev_001@mail.ru>
2022-11-18 20:14:23 +0300

    Добавлено разбиение на страницы.

--- SportsStore/Controllers/HomeController.cs
+++ SportsStore/Controllers/HomeController.cs
@@ -6,15 +6,19 @@ namespace SportsStore.Controllers
 	public class HomeController : Controller
 	{
 		private IStoreRepository repository;
+		public int pageSize = 4;
 
 		public HomeController(IStoreRepository repo)
 		{
 			repository = repo;
 		}
 
-		public IActionResult Index()
+		public IActionResult Index(int productPage = 1)
 		{
-			return View(repository.Products);
+			return View(repository.Products.
+				OrderBy(p => p.ProductId).
+				Skip((productPage - 1) * pageSize).
+				Take(pageSize));
 		}
 	}
 }
