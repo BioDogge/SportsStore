33dfe85e3f2dd9df4c720adf0f77884cab54f930
Алексей Манаев <manaev_001@mail.ru>
2022-11-18 18:31:50 +0300

    Добавил необходимые настройки для EF, сервис и начальные данные для БД.

--- SportsStore/Program.cs
+++ SportsStore/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using SportsStore.Models;
+
 namespace SportsStore
 {
 	public class Program
@@ -5,13 +8,22 @@ namespace SportsStore
 		public static void Main(string[] args)
 		{
 			var builder = WebApplication.CreateBuilder(args);
+
 			builder.Services.AddControllersWithViews();
+			builder.Services.AddDbContext<StoreDbContext>(options =>
+			{
+				options.UseSqlServer(builder.Configuration["ConnectionStrings:SportsStoreConnection"]);
+			});
+			builder.Services.AddScoped<IStoreRepository, EFStoreRepository>();
+
 			var app = builder.Build();
 
 			//app.MapGet("/", () => "Hello World!");
 
 			app.UseStaticFiles();
 			app.MapDefaultControllerRoute();
+
+			SeedData.EnsurePopulated(app);
 
 			app.Run();
 		}
