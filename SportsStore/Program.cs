a4caf945356c373e65f7a29bb043e8fb2789e525
Алексей Манаев <manaev_001@mail.ru>
2022-11-18 16:30:34 +0300

    Произведена начальная конфигурация приложения.

--- SportsStore/Program.cs
+++ SportsStore/Program.cs
@@ -5,9 +5,13 @@ namespace SportsStore
 		public static void Main(string[] args)
 		{
 			var builder = WebApplication.CreateBuilder(args);
+			builder.Services.AddControllersWithViews();
 			var app = builder.Build();
 
-			app.MapGet("/", () => "Hello World!");
+			//app.MapGet("/", () => "Hello World!");
+
+			app.UseStaticFiles();
+			app.MapDefaultControllerRoute();
 
 			app.Run();
 		}
