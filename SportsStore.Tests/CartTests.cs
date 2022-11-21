1e2e26a53899488411bd39fc2df6ba331d930fe7
Алексей Манаев <manaev_001@mail.ru>
2022-11-21 22:31:39 +0300

    Добавлены модель и функциональности корзины, также юнит-тесты для них.

---
+++ SportsStore.Tests/CartTests.cs
@@ -0,0 +1,94 @@
+﻿
+namespace SportsStore.Tests
+{
+	public class CartTests
+	{
+		[Fact]
+		public void CanAddNewLines()
+		{
+			Product p1 = new Product { ProductId = 1, Name = "P1" };
+			Product p2 = new Product { ProductId = 2, Name = "P2" };
+
+			Cart target = new Cart();
+
+			target.AddItem(p1, 1);
+			target.AddItem(p2, 1);
+			CartLine[] result = target.Lines.ToArray();
+
+			Assert.Equal(2, result.Length);
+			Assert.Equal(p1, result[0].Product);
+			Assert.Equal(p2, result[1].Product);
+		}
+
+		[Fact]
+		public void CanAddQuantityForExistingLines()
+		{
+			Product p1 = new Product { ProductId = 1, Name = "P1" };
+			Product p2 = new Product { ProductId = 2, Name = "P2" };
+
+			Cart target = new Cart();
+
+			target.AddItem(p1, 1);
+			target.AddItem(p2, 1);
+			target.AddItem(p1, 10);
+
+			CartLine[] result = target.Lines.ToArray();
+
+			Assert.Equal(2, result.Length);
+			Assert.Equal(11, result[0].Quantity);
+			Assert.Equal(1, result[1].Quantity);
+		}
+
+		[Fact]
+		public void CanRemoveLine()
+		{
+			Product p1 = new Product { ProductId = 1, Name = "P1" };
+			Product p2 = new Product { ProductId = 2, Name = "P2" };
+			Product p3 = new Product { ProductId = 3, Name = "P3" };
+
+			Cart target = new Cart();
+
+			target.AddItem(p1, 1);
+			target.AddItem(p2, 3);
+			target.AddItem(p3, 1);
+			target.AddItem(p2, 2);
+
+			target.RemoveLine(p2);
+
+			Assert.Empty(target.Lines.Where(p => p.Product == p2));
+			Assert.Equal(2, target.Lines.Count());
+		}
+
+		[Fact]
+		public void CalculateCartTotal()
+		{
+			Product p1 = new Product { ProductId = 1, Name = "P1", Price = 100M };
+			Product p2 = new Product { ProductId = 2, Name = "P2", Price = 50M };
+
+			Cart target = new Cart();
+
+			target.AddItem(p1, 1);
+			target.AddItem(p2, 1);
+
+			decimal result = target.ComputeTotalValue();
+
+			Assert.Equal(150, result);
+		}
+
+		[Fact]
+		public void CanClearLines()
+		{
+			Product p1 = new Product { ProductId = 1, Name = "P1" };
+			Product p2 = new Product { ProductId = 2, Name = "P2" };
+
+			Cart target = new Cart();
+
+			target.AddItem(p1, 1);
+			target.AddItem(p2, 1);
+
+			target.Clear();
+
+			Assert.Empty(target.Lines);
+		}
+	}
+}
--- SportsStore/Program.cs
+++ SportsStore/Program.cs
@@ -14,13 +14,19 @@ namespace SportsStore
 			{
 				options.UseSqlServer(builder.Configuration["ConnectionStrings:SportsStoreConnection"]);
 			});
+
 			builder.Services.AddScoped<IStoreRepository, EFStoreRepository>();
+
+			builder.Services.AddRazorPages();
+			builder.Services.AddDistributedMemoryCache();
+			builder.Services.AddSession();
 
 			var app = builder.Build();
 
 			//app.MapGet("/", () => "Hello World!");
 
 			app.UseStaticFiles();
+			app.UseSession();
 
 			app.MapControllerRoute("catpage", "{category}/Page{productPage:int}", 
 				new {Controller = "Home", action = "Index"});
@@ -35,6 +41,7 @@ namespace SportsStore
 				new { Controller = "Home", action = "Index", productPage = 1 });
 
 			app.MapDefaultControllerRoute();
+			app.MapRazorPages();
 
 			//SeedData.EnsurePopulated(app);
 
